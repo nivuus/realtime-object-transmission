fa5c4ab21b6776aeb6d0732b9a5d9a94c3a39292
Maxime Allanic <maxime@allanic.me>
2020-03-29 18:42:48 +0200

    [FIX] lot of bugs

--- test/index.js
+++ test/index.js
@@ -8,14 +8,14 @@
 const $realtimeObjectTransmission = require('../');
 const $socketClient = require('socket.io-client');
 const $assert = require('assert');
-const $fs = require('fs');
-const $crypto = require('crypto');
+const Proxify = require('@nivuus/proxify');
+const $http = require('http');
 
 describe('RealtimeObjectTransmission', () => {
     var app, server, client;
 
     beforeEach(() => {
-        app = require('http').createServer();
+        app = $http.createServer();
         server = new $realtimeObjectTransmission.Server(app);
         app.listen(80);
         client = new $realtimeObjectTransmission.Client($socketClient('http://localhost'));
@@ -28,8 +28,8 @@ describe('RealtimeObjectTransmission', () => {
     });
 
     it('should transfer data', (done) => {
-        server.onConnection(function (session) {
-            var t = session.newObject();
+        server.onConnection(async function (session) {
+            var t = await session.newObject();
             t.$watch('t', (value) => {
                 $assert.equal(value, 'test');
                 done();
@@ -37,16 +37,16 @@ describe('RealtimeObjectTransmission', () => {
         });
 
 
-        client.connect((session) => {
-            session.onNewObject((o) => {
-                o.t = 'test';
-            });
+        var session = client.connect();
+
+        session.onNewObject((o) => {
+            o.t = 'test';
         });
     });
 
     it('should call method', (done) => {
-        server.onConnection(function (session) {
-            var t = session.newObject({
+        server.onConnection(async function (session) {
+            await session.newObject({
                 o: function (value) {
                     $assert.equal(value, 'test');
                     done();
@@ -54,51 +54,68 @@ describe('RealtimeObjectTransmission', () => {
             });
         });
 
-        client.connect((session) => {
-            session.onNewObject((o) => {
-                o.o('test');
-            });
+        var session = client.connect();
+        session.onNewObject((o) => {
+            o.o('test');
         });
-
     });
 
     it('should response method', (done) => {
-        server.onConnection(function (session) {
-            var t = session.newObject({
+        server.onConnection(async function (session) {
+            var t = await session.newObject({
                 o: function (value) {
                     return 'test2';
                 }
             });
         });
 
-        client.connect((session) => {
-            session.onNewObject((o) => {
-                o.o('test')
-                    .then((value) => {
-                        $assert.equal(value, 'test2');
-                        done();
-                    });
-            });
+        var session = client.connect();
+        session.onNewObject((o) => {
+            o.o('test')
+                .then((value) => {
+                    $assert.equal(value, 'test2');
+                    done();
+                });
         });
     });
 
     it('should throw response method', (done) => {
-        server.onConnection(function (session) {
-            var t = session.newObject({
+        server.onConnection(async function (session) {
+            var t = await session.newObject({
                 o: function (value) {
                     throw new Error('test');
                 }
             });
         });
 
-        client.connect((session) => {
-            session.onNewObject((o) => {
-                o.o('test')
-                    .catch((value) => {
-                        $assert.equal(value, 'Error: test');
-                        done();
-                    });
+        var session = client.connect();
+        session.onNewObject((o) => {
+            o.o('test')
+                .catch((value) => {
+                    $assert.equal(value, 'Error: test');
+                    done();
+                });
+        });
+    });
+
+    it('should proxify args', (done) => {
+        server.onConnection(async function (session) {
+            session.on('rest', (proxified) => {
+                $assert.ok(Proxify.isProxy(proxified));
+                done();
+            });
+            var t = await session.newObject({
+                o: function (value) {
+                    throw new Error('test');
+                }
             });
         });
+
+        var session = client.connect();
+        var proxified = new Proxify({});
+        session.emit('rest', proxified)
+            .then(() => {
+
+            });
     });
 });
